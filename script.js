9b74d47d7f0c3ac3d16027e49848468530d3c8db
Raphael Sartori <raphaelgatto61@gmail.com>
2023-10-25 10:35:25 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+    const htlm = document.documentElement
+htlm.classList.toggle('light')
+
+
+const img = document.querySelector("#profile img")
+
+if(htlm.classList.contains('light')) {
+
+    img.setAttribute('src', './assets/avatar-light.png')
+} else {
+    img.setAttribute('src', './assets/avatar.png')
+}
+
+}
