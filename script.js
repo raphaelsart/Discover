b094331f11fb9e01f7a45e4321dde46a1fa80054
Raphael Sartori <raphaelgatto61@gmail.com>
2023-10-27 08:05:58 -0300

    Personalização final de fotos e links

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ if(htlm.classList.contains('light')) {
 
     img.setAttribute('src', './assets/avatar-light.png')
 } else {
-    img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute('src', './assets/avatar-dark.png')
 }
 
 }
